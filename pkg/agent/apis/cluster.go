821153f505b2986e7aeefb69ade3b1e64f1336f9
yu.deng <dengyu326@gmail.com>
2022-09-06 03:03:35 +0800

    enhancement: cache api-resources

--- pkg/agent/apis/cluster.go
+++ pkg/agent/apis/cluster.go
@@ -1,14 +1,21 @@
 package apis
 
 import (
+	"sync"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/discovery"
 	"kubegems.io/kubegems/pkg/agent/cluster"
 	"kubegems.io/kubegems/pkg/log"
 )
 
 type ClusterHandler struct {
-	cluster cluster.Interface
+	cluster        cluster.Interface
+	lastUpdateTime *time.Time
+	rwmu           sync.RWMutex
+	cacheData      []*v1.APIResourceList
 }
 
 // @Tags         Agent.V1
@@ -20,10 +27,14 @@ type ClusterHandler struct {
 // @Success      200      {object}  handlers.ResponseStruct{Data=[]object}  "resp"
 // @Router       /v1/proxy/cluster/{cluster}/api-resources [get]
 func (h *ClusterHandler) APIResources(c *gin.Context) {
-	ret, err := h.cluster.Discovery().ServerPreferredResources()
+	ret, refreshed, err := h.getApiResources()
+	if refreshed {
+		h.rwmu.Lock()
+		defer h.rwmu.Unlock()
+		h.cacheData = ret
+	}
 	if err != nil {
 		if discovery.IsGroupDiscoveryFailedError(err) {
-			log.Warnf("get api-resources failed: %v", err)
 			OK(c, ret)
 			return
 		} else {
@@ -32,4 +43,18 @@ func (h *ClusterHandler) APIResources(c *gin.Context) {
 		}
 	}
 	OK(c, ret)
+}
+
+func (h *ClusterHandler) getApiResources() (ret []*v1.APIResourceList, refreshed bool, err error) {
+	if time.Since(*h.lastUpdateTime) > time.Duration(time.Minute*5) && len(h.cacheData) > 0 {
+		ret = h.cacheData
+		return
+	}
+	ret, err = h.cluster.Discovery().ServerPreferredResources()
+	if err != nil {
+		log.Error(err, "failed to refresh api-resources")
+		return
+	}
+	refreshed = true
+	return
 }
